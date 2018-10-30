c723ffb0693a0603ba55fd5deae14147aed559d4
puzzle <puzzle@192.168.1.105>
2018-10-30 23:30:53 +0800

    apply zuul

---
+++ demo-zuul/src/main/java/com/xxxx/pb/DemoZuulApplication.java
@@ -0,0 +1,22 @@
+package com.xxxx.pb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.xxxx.pb.demo.zuul.filter.AccessFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class DemoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZuulApplication.class, args);
+	}
+	
+	@Bean
+	public AccessFilter accessFilter() {
+	    return new AccessFilter();
+	}
+}
