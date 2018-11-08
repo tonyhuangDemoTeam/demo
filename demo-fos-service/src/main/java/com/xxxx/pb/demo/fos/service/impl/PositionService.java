47b34e43172a8d7bced524822a9fd1d979ef4f09
puzzle <puzzle@131.163.136.219.broad.gz.gd.dynamic.163data.com.cn>
2018-11-08 22:35:57 +0800

    update version to 1.0

---
+++ demo-fos-service/src/main/java/com/xxxx/pb/demo/fos/service/impl/PositionService.java
@@ -0,0 +1,8 @@
+package com.xxxx.pb.demo.fos.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PositionService {
+    public List<Map<String, String>> getPositions();
+}
---
+++ demo-fos-service/src/main/java/com/xxxx/pb/demo/fos/service/impl/PositionServiceImpl.java
@@ -0,0 +1,14 @@
+package com.xxxx.pb.demo.fos.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+public class PositionServiceImpl implements PositionService {
+
+    @Override
+    public List<Map<String, String>> getPositions() {
+        
+        return null;
+    }
+
+}
